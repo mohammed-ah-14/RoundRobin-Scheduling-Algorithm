bf6d224ba4d689d6098447c078aa4e120b9f3304
Mohammed Haris <mohammed.abdulharis@outlook.com>
2026-03-16 22:22:22 +0400

    Initial commit - skeleton code

---
+++ G8_OS_HW1.cpp
@@ -0,0 +1,38 @@
+// OS Homework 1 - Round Robin Scheduling
+// TODO
+/*  
+    Group 8: 
+    - Member 1: Mohammed Abdul Haris - U23100877
+    - Member 2: [Name] - [ID]
+    - Member 3: [Name] - [ID]
+    - Member 4: [Name] - [ID]
+    - Member 5: [Name] - [ID]
+*/
+ 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // ----- Member 1 : Input & Validation -----
+
+
+
+
+    // ----- Member 2 : Round Robin Logic -----
+
+
+
+
+    // ----- Member 3 : Waiting Time Calculation -----
+
+    
+
+    // ----- Member 4 : Turnaround Time & Averages -----
+
+    // ----- Member 5 : Output Formatting -----
+
+    return 0;
+}
