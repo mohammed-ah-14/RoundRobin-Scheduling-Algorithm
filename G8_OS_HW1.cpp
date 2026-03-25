76189208f6753dad1adfc9de8939b15985ae85e2
Mohammed Haris <mohammed.abdulharis@outlook.com>
2026-03-25 21:06:30 +0400

    line by line explanation added

--- G8_OS_HW1.cpp
+++ G8_OS_HW1.cpp
@@ -21,34 +21,34 @@ int main(){
     int burstTime[10], prcs, quantum;
     int arrivalTime = 0; // we are assuming all processes arrive at time 0 for simplicity
 
-    cout << "\n===== Round Robin Scheduling =====\n";
 
     do
     {
-        cout << "\nEnter number of processes (1-10): ";
+        cout << "\nEnter No of Processes: "; // gets the number of processes to be scheduled, must be between 1 and 10
         cin >> prcs;
+        cout << endl;
         if (prcs < 1 || prcs > 10)
             cout << "Invalid! Number of processes must be between 1 and 10.\n";
     } while (prcs < 1 || prcs > 10);
 
-    cout<<endl;
+    
 
-    for (int i = 0; i < prcs; i++)
+    for (int i = 0; i < prcs; i++) // for each process, get the burst time (CPU time needed), must be greater than 0
     {
         do
         {
-            cout << "Enter burst time for process P" << (i + 1) << ": ";
+            cout << "P[" << (i + 1) << "]: ";
             cin >> burstTime[i];
+            cout << endl;
             if (burstTime[i] <= 0)
                 cout << "Invalid! Burst time must be greater than 0.\n";
         } while (burstTime[i] <= 0);
     }
 
-    cout<<endl;
 
     do
     {
-        cout << "Enter time quantum: ";
+        cout << "Enter Time Quantum: "; // gets the time quantum (time slot for each process), must be greater than 0
         cin >> quantum;
         if (quantum <= 0)
             cout << "Invalid! Time quantum must be greater than 0.\n";
@@ -58,7 +58,7 @@ int main(){
     // ----- Member 2 : Round Robin Logic -----  Mohammed Haris
 
     int remainingTime[10];  // remaining burst time per process
-    int completionTime[10]; 
+    int completionTime[10]; // time at which each process completes execution
     int currentTime = 0;    // current time on the CPU clock
     bool done = false;
 
@@ -66,26 +66,25 @@ int main(){
     for (int i = 0; i < prcs; i++)
         remainingTime[i] = burstTime[i];
 
-    // Keep cycling through processes until all are done
+    // Keep cycling through processes until all processes are done
     while (!done)
     {
-        done = true; // Assume all are done
+        done = true; // Assume all processes are done
 
         for (int i = 0; i < prcs; i++)
         {
-            if (remainingTime[i] > 0) // Process still has CPU work left
+            if (remainingTime[i] > 0) // if there is still burst time left for this process that means it's not done
             {
-                done = false; // At least one process is not done yet
+                done = false; // so we flag the variable to false to indicate we still have processes to execute
 
-                if (remainingTime[i] > quantum)
+                if (remainingTime[i] > quantum) // if remaining time is greater than the quantum, we cant execute the process in one go, 
+                                                // so we execute it for one quantum and then put it back in the queue
                 {
-                    // Process runs for a full quantum, then goes back to queue
                     currentTime += quantum;
                     remainingTime[i] -= quantum;
                 }
                 else
-                {
-                    // Process finishes within this quantum slot
+                { // if remaining time is less than or equal to the quantum, we can execute the process in one go and it will finish
                     currentTime += remainingTime[i];
                     remainingTime[i] = 0;
                     completionTime[i] = currentTime; // Record finish time
@@ -97,7 +96,7 @@ int main(){
 
     // ----- Member 3 :  Turnaround Time  -----  Azhar
 
-    int turnaroundTime[10];
+    int turnaroundTime[10];     // turnaround time per process (time from arrival to completion)
     for (int i = 0; i < prcs; i++) {
         turnaroundTime[i] = completionTime[i] - arrivalTime;
     }
@@ -105,36 +104,36 @@ int main(){
 
     // ----- Member 4 :  Waiting Time Calculation & Averages -----  Faris
 
-    int waitingTime[10];
+    int waitingTime[10];        
     double totalWaitTime = 0;
     double totalTurnaroundTime = 0;
 
     for (int i = 0; i < prcs; i++) {
-        waitingTime[i] = turnaroundTime[i] - burstTime[i];
+        waitingTime[i] = turnaroundTime[i] - burstTime[i]; // waiting time for each process (time spent waiting in the ready queue, calculated as turnaround time - burst time)
         totalWaitTime += waitingTime[i];
         totalTurnaroundTime += turnaroundTime[i];
     }
 
-    double avgWaitTime = totalWaitTime/prcs;
-    double avgTurnaroundTime = totalTurnaroundTime/prcs;
+    double avgWaitTime = totalWaitTime/prcs;    // average waiting time across all processes
+    double avgTurnaroundTime = totalTurnaroundTime/prcs; // average turnaround time across all processes
 
 
     // ----- Member 5 : Output Formatting -----  Ahmad
 
-    cout << "\n=====  Result =====\n";
+    // cout << "\n=====  Result =====\n";
     // cout << "\nProcess\t\tBurst Time\tCompletion Time\t\tTurnaround Time\t\tWaiting Time\n";
     cout << "\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time\n";
 
     for (int i = 0; i < prcs; i++)
     {
-        cout << "P" << (i + 1) << "\t\t"
+        cout <<(i + 1) << "\t\t"
              << burstTime[i] << "\t\t"
              //  << completionTime[i] << "\t\t\t"
              << waitingTime[i] << "\t\t"
              << turnaroundTime[i] << "\n";
     }
 
-    cout << "\nAverage Waiting Time: " << avgWaitTime << endl;
+    cout << "Average Waiting Time: " << avgWaitTime << endl;
     cout << "Average Turnaround Time: " << avgTurnaroundTime << endl;
     cout<<endl;
 
